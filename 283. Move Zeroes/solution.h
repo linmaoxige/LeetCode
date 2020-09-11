2ffbe28bc1a56c1b5886784aaef764b779a34319
linmaoxige <748893439@qq.com>
2020-09-11 09:28:39 +0800

    update

---
+++ 283. Move Zeroes/solution.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt = 0;
+        for (auto it = nums.begin(); it != nums.end(); it++) {
+            if (*it == 0) {
+                cnt++;
+                nums.erase(it);
+                it--;
+            }
+        }
+        for (int i = 0; i < cnt; i++) {
+            nums.push_back(0);
+        }
+    }
+};
